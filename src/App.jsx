cb07af5147fb060f04b39ecca94c019a588f4039
Juan <jj.solarte26@ciaf.edu.co>
2025-10-01 21:09:10 -0500

    aPP.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
-
+import { Card } from './components/Card'
+import { useEffect } from 'react';
 function App() {
   
+  useEffect(() =>{
+    fetch('http://localhost:3000/products')
+    .then(response => response.json())
+    .then(data => console.log(data))
+  },[]);
 
   return (
     <>
-    <h1> mi primera app en react</h1>
+    <h1> Productos </h1>
+    <Card/>
     </>
   )
 }
