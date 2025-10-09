5784474ce8665636deb8915ed3f5b86234a6b7ac
Juan <jj.solarte26@ciaf.edu.co>
2025-10-08 21:07:53 -0500

    Front-end

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
 import './App.css'
 import { Card } from './components/Card'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 function App() {
-  
-  useEffect(() =>{
-    fetch('http://localhost:3000/products')
-    .then(response => response.json())
-    .then(data => console.log(data))
-  },[]);
+  const [products, setProducts] = useState([]);
 
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  const getProducts = async () => {
+    const response = await fetch("http://localhost:3001/products");
+    const data = await response.json();
+    const products = data.products;
+    setProducts(products);
+    console.log(products);
+  };
   return (
     <>
-    <h1> Productos </h1>
-    <Card/>
+      <h1> Productos </h1>
+      {products.map((product) => (
+        <div className="container">
+          <h1 className="title">Productos</h1>
+          {products.map((product) => (
+            <div key={product.id} className="product-card">
+              <h2 className="product-name">{product.name}</h2>
+              <p className="product-price">{product.price}</p>
+            </div>
+          ))}
+        </div>
+      ))}
+      <Card />
     </>
   )
 }
